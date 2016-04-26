9397c27d5b072c9815a06b012ac32f4a49f21783
alekzandre carayon <a.p.carayon@gmail.com>
2016-04-26 20:01:42 +0200

    start ajax

--- index.py
+++ index.py
@@ -3,11 +3,14 @@ from flask.ext.bootstrap import Bootstrap
 from flask.ext.script import Manager
 from flask.ext.moment import Moment
 from datetime import datetime
+import facebook, urllib2, json
 
 app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+# gestion des pages d'erreurs
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -15,12 +18,30 @@ def page_not_found(e):
 
 @app.errorhandler(500)
 def page_not_found(e):
-	return render_template('500.html'),500
+	return render_template('500.html'), 500
+
+# routes principales
+
+def update_stats():
+	print "fuck"
+	response = urllib2.urlopen('https://www.en-marche.fr/ajax.php?action=stats42')
+	try:
+		data = json.load(response)
+		adherents = data['a']
+		marcheurs = data['i']
+	except:
+		return ('error while fetching data')
+	return (marcheurs, adherents)
 
 @app.route('/')
 def index():
-	user_agent = request.headers.get('User-Agent')
-	return render_template('index.html', ue=user_agent, current_time=datetime.utcnow())
+	try:
+		data = update_stats()
+		adherents = data[0]
+		marcheurs = data[1]
+	except:
+		return render_template('index.html', current_time=datetime.utcnow())
+	return render_template('index.html', current_time=datetime.utcnow(), ad=adherents, ma=marcheurs)
 
 if __name__ == '__main__':
 	manager.run()
