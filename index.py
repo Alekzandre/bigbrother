0e489b41d54b8146252857deead764aa810955a0
alekzandre carayon <a.p.carayon@gmail.com>
2016-04-28 22:38:31 +0200

    add api

--- index.py
+++ index.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify, redirect, url_for
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.script import Manager
 from flask.ext.moment import Moment
 from datetime import datetime
-import facebook, urllib2, json
+import facebook, urllib2, json, MySQLdb, os
 
 app = Flask(__name__)
 manager = Manager(app)
@@ -20,7 +20,54 @@ def page_not_found(e):
 def page_not_found(e):
 	return render_template('500.html'), 500
 
-# routes principales
+# utils
+
+def connect_to_db_prod():
+	json_conf = open('db.conf')
+	confData = json.loads(str(json_conf.read()))
+	db = MySQLdb.connect(host=confData['host']['host_enmarche'],
+						user=confData['user']['user_enmarche'],
+						passwd=confData['pass']['pass_enmarche'],
+						db=confData['name']['name_enmarche'])
+	return db
+
+@app.route('/v0.0/today_ad')
+def load_stats():
+	db = connect_to_db_prod()
+	cur = db.cursor()
+	result = cur.execute("select count(*) from mac_inscriptions where created_at > '2016-04-27 22:00:00' and created_at < '2016-04-28 22:00:00' and adherent = 1;")
+	if result == 1:
+		for row in cur.fetchall():
+   			value = row[0]
+   			print value
+   			return jsonify({'today_ad':value})
+   	return "<h1>coucou</h1>"
+
+@app.route('/v0.0/all_stats')
+def load_stats_bis():
+	db = connect_to_db_prod()
+	cur = db.cursor()
+	result = cur.execute("select count(*) from mac_inscriptions;")
+	if result == 1:
+		for row in cur.fetchall():
+   			value = row[0]
+   			print value
+   	result = cur.execute("select count(*) from mac_inscriptions where adherent = 1;")
+	if result == 1:
+		for row in cur.fetchall():
+   			value1 = row[0]
+   			print value1
+   			res = [{'total':value},{'adherents':value1}]
+   			return jsonify(results=res)
+   	return "<h1>coucou</h1>"
+
+@app.route('/v0.0/test')
+def test():
+	pass	
+
+	# start_date = date( 'Y-m-d 00:00:00', $start );
+	# $end_date   = date( 'Y-m-d 00:00:00', $end   );
+
 
 def update_stats():
 	response = urllib2.urlopen('https://www.en-marche.fr/ajax.php?action=stats42')
@@ -32,14 +79,15 @@ def update_stats():
 		return ('error while fetching data')
 	return (marcheurs, adherents)
 
+# route principales
+
 @app.route('/')
 def index():
-	try:
-		data = update_stats()
-		adherents = data[1]
-		marcheurs = data[0]
-	except:
-		return render_template('index.html', current_time=datetime.utcnow())
+	#db = connect_to_db_prod()
+	# load_stats(db)
+	data = update_stats()
+	adherents = data[1]
+	marcheurs = data[0]
 	return render_template('index.html', current_time=datetime.utcnow(), ad=adherents, ma=marcheurs)
 
 if __name__ == '__main__':
